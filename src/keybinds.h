19c2eb36e7b8f996cb52477c4e7ded7cc0f27ad4
Sebastian Parborg <darkdefende@gmail.com>
2010-06-23 16:46:30 +0800

    Forgot to add a header file.

---
+++ src/keybinds.h
@@ -0,0 +1,22 @@
+/* This file contains the key bindings */
+
+#include <stdio.h>
+
+int upkey;
+int downkey;
+int leftkey;
+int rightkey;
+int strafeleft;
+int straferight;
+int strafekey;
+int walkkey;
+int attackkey;
+int changewep;
+int pausekey;
+int linekey;
+int musickey;
+int noteskey;
+int redkey;
+int greenkey;
+int bluekey;
+int yellowkey;
